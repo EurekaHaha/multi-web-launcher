c0d64af55c9e234c3b4d88ed3525569223725428
EurekaHaha <wangzhaosafe@163.com>
2025-03-14 21:13:26 +0800

    feat: Add logging functionality to NodeLog struct

---
+++ src/log.rs
@@ -0,0 +1,62 @@
+use crate::project::{ Project, Status };
+use std::io::{ BufReader, BufRead };
+use std::collections::VecDeque;
+use std::thread;
+
+#[allow(unused)]
+pub struct NodeLog<'a> {
+    pub project: &'a mut Project,
+    pub log_list: VecDeque<String>,
+    pub status: Status,
+    max_logs: usize,
+}
+
+impl<'a> NodeLog<'a> {
+    pub fn set_status(&mut self, status: Status) {
+        self.status = status;
+    }
+}
+
+impl<'a> NodeLog<'a> {
+    pub fn new(project: &'a mut Project) -> Self {
+        Self {
+            project,
+            log_list: VecDeque::with_capacity(2000),
+            status: Status::Unknown,
+            max_logs: 2000,
+        }
+    }
+
+    pub fn log_start(&mut self) {
+        let process: &mut Option<std::process::Child> = &mut self.project.process;
+
+        println!("process: {:?}", process);
+
+        match process {
+            Some(ref mut process) => {
+                // @ 每个流只能take一次 take之后流的所有权就转移了
+                let reader = if let Some(stdout) = process.stdout.take() {
+                    BufReader::new(stdout)
+                } else {
+                    self.project.stop().unwrap();
+                    panic!("stdout is None");
+                };
+
+                let project_name = self.project.name.clone();
+                self.set_status(Status::Running);
+
+                thread::spawn(move || {
+                    for line in reader.lines() {
+                        if let Ok(line) = line {
+                            // todo 这里之后不会直接打印到控制台 而是打印到一个新的UI中
+                            println!("[{}]输出: {}", project_name, line);
+                        }
+                    }
+                });
+            },
+            None => {
+                println!("process is None");
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod project;
 mod config;
+mod log;
 
 use std::io::*;
-use std::time::Duration;
 
 fn main() {
+    // * 测试启动
+
     let project_config = config::ProjectConfig::new();
     // 创建 proxy 项目实例
     let mut proxy = project::Project::new(&project_config);
@@ -12,6 +14,10 @@ fn main() {
     // 启动 proxy
     println!("正在启动 proxy...");
     proxy.start().expect("启动 proxy 失败");
+    println!("proxy 启动成功");
+
+    let mut proxy_log: log::NodeLog<'_> = log::NodeLog::new(&mut proxy);
+    proxy_log.log_start();
 
     let stdin = std::io::stdin();
     for line in stdin.lock().lines() {
